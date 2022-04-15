bc17fa2504004fb8312cfe9c2457a477e3b2f56a
Vưu Trí Tấn <99905204+vuutritan2708@users.noreply.github.com>
2022-04-15 13:10:39 +0700

    Add project files.

---
+++ js/tp.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar-fixed-top");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
